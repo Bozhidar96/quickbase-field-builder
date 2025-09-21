be176e8f5efdfcda62c4d2112b24530de8ac0bd8
bozhidar.rusev <bojidar_rusev96@abv.bg>
2025-09-21 09:09:37 +0300

    Project setup

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    trailingComma: 'none',
+    semi: false,
+    tabWidth: 4,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderCaseInsensitive: true,
+    printWidth: 120,
+    endOfLine: 'auto',
+    arrowParens: 'always',
+    bracketSpacing: true
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import FieldBuilder from "./components/FieldBuilder";
+import { FieldConfig } from "./types";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <span>Bozhidar Here</span>
+    </div>
+  );
+}
+
+export default App;
